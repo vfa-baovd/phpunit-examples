4545c035dee40e021ee03388b37fe490b0186cb1
BaoVo <baovd@vitalify.asia>
2017-12-29 10:58:22 +0700

    test fail

--- tests/SimpleTest.php
+++ tests/SimpleTest.php
@@ -8,7 +8,7 @@ class SimpleTest extends \PHPUnit_Framework_TestCase
 		$simple = new Simple(10);
 		$result = $simple->divide(2);
 
-		$this->assertEquals(5, $result);
+		$this->assertEquals(4, $result);
 	}
 
     /**
